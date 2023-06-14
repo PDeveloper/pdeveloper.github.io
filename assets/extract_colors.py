1ca88990451995ff04537dd0e9595f7ddbf233a8
PDeveloper <2379133+PDeveloper@users.noreply.github.com>
2023-06-15 02:05:21 +0300

    Upgraded the color extraction

--- assets/extract_colors.py
+++ assets/extract_colors.py
@@ -7,36 +7,32 @@ def parse_scss_file(input_file, output_file):
         scss_content = file.read()
 
     # Find color values using regular expression
-    color_pattern = r'(#(?:[0-9a-fA-F]{3}){1,2}|rgba?\([^)]+\)|hsva?\([^)]+\)|hsla?\([^)]+\))'
-    color_matches = re.findall(color_pattern, scss_content)
+    color_pattern = r'(#(?:[0-9a-fA-F]{3}){1,2}|rgb(?:a)?\([^)]+\)|hsv(?:a)?\([^)]+\)|hsl(?:a)?\([^)]+\))'
+    color_matches = re.finditer(color_pattern, scss_content)
 
-    # Remove duplicates and sort color values
-    color_values = sorted(set(color_matches))
+    # Store color positions and values
+    color_positions = []
+    color_values = []
 
-    # Extract block names from SCSS content
-    block_pattern = r'(?<=\}).*?(?=\{)'
-    block_names = re.findall(block_pattern, scss_content, re.DOTALL)
+    # Iterate over color matches
+    for match in color_matches:
+        color_positions.append(match.span())
+        color_values.append(match.group())
 
-    # Generate variable names for colors based on block names
-    color_variables = []
-    variable_index = 0
-    for block in block_names:
-        block = block.strip().lower()
-        for index, color in enumerate(color_values):
-            variable_name = '{}Color{}'.format(block, index)
-            if variable_name not in color_variables:
-                color_variables.append(variable_name)
-            else:
-                variable_name = '{}Color{}'.format(block, variable_index)
-                color_variables.append(variable_name)
-                variable_index += 1
+    # Generate variable names for colors
+    color_variables = ['${}'.format(variable) for variable in ['color{}'.format(index) for index in range(len(color_values))]]
 
     # Replace color occurrences with generated variables
-    for color, variable in zip(color_values, color_variables):
-        scss_content = scss_content.replace(color, '${}'.format(variable))
+    offset = 0
+    for position, variable in zip(color_positions, color_variables):
+        start, end = position
+        start += offset
+        end += offset
+        scss_content = scss_content[:start] + variable + scss_content[end:]
+        offset += len(variable) - (end - start)
 
     # Generate variables section at the top of the file
-    variables_section = ''.join(['${}: {};\n'.format(variable, color) for variable, color in zip(color_variables, color_values)])
+    variables_section = ''.join(['{}: {};\n'.format(variable, color) for variable, color in zip(color_variables, color_values)])
 
     # Insert variables section at the top of the file
     scss_content = variables_section + scss_content
